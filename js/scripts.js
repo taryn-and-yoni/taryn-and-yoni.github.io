695b95abaa703be8f6ee642aca49e58a77226529
taryn-and-yoni <92409611+taryn-and-yoni@users.noreply.github.com>
2022-03-02 06:39:53 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Taryn and Yoni's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Oct 02, 2022 16:00'),
 
             // Event duration (IN MINUTES)
-            // duration: 120,
+            // duration: 240,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Oct 02, 2022 22:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '2 Lighthouse Rd, New Haven, CT 06512',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day!."
         }
     });
 
